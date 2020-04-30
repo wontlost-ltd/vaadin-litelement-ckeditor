cc3a5e39fb8d25d3b8d80dc07c21255e418d9f9e
Ryan Pang <ryan.pang@live.com>
2020-05-01 00:39:50 +1200

    Highlight

--- src/main/resources/META-INF/frontend/vaadin-ckeditor.js
+++ src/main/resources/META-INF/frontend/vaadin-ckeditor.js
@@ -1,7 +1,11 @@
 import { LitElement, html } from "lit-element";
 import { classMap } from 'lit-html/directives/class-map';
-import { initHighlightingOnLoad,highlightAuto,highlightBlock,initHighlighting } from 'highlight.js';
 import { ClassicEditor, InlineEditor, BalloonEditor, DcoupledEditor } from "./ckeditor";
+
+const hljs = require('highlight.js');
+// Or using the CommonJS version:
+// const ClassicEditor = require( '@ckeditor/ckeditor5-build-classic' );
+let _code_languages = ["plain","c","cs","cpp","html","xml","css","javascript","python","sql","php","perl","ruby","markdown","auto"];
 
 class VaadinCKEditor extends LitElement {
 
@@ -93,7 +97,30 @@ class VaadinCKEditor extends LitElement {
         initHighlightingOnLoad();
         if(this.editorType==='classic') {
             ClassicEditor.create( document.querySelector( '#classic-editor' ) , {
-                    toolbar:this.toolBar
+                    toolbar:this.toolBar,
+                    preCodeBlock :{
+
+                        languages: _code_languages.map( _language => {return{
+                            language: _language,
+                            title: _language=="cs"?"c#":_language
+                        };}),
+                        toolbar: [ 'EditLanguage', '|', 'SelectLanguage' , '|', 'HighlightCodeBlock', '|', 'CloseCodeBlock'],
+                        noOfSpaceForTabKey: 4,
+                        highlightConfig:{
+                            // this function is called whenever syntax highlighting is requested.
+                            // the highlighting pre element and language will be the arguments for this function
+                            // this function should return syntax highlighted block.
+                            // below example uses highlightjs as syntax highlighter
+                            highlighter:( pre_block, language )=>{
+
+                                // to undo highlighting simply uncomment below two lines and return pre_block as is
+                                // or you can select plain/text/nohighlight option from language if highlighter supports
+                                pre_block.innerHTML = pre_block.innerHTML.replace(/<br[ \/]*>/gi, '\n');
+                                hljs.highlightBlock(pre_block); // refer https://github.com/highlightjs/highlight.js
+                                return pre_block; // return highlighted pre block to plugin
+                            }
+                        }
+                    }
                 }).then( editor => {
                     editor.isReadOnly = this.isReadOnly;
                     editor.setData(this.editorData);
