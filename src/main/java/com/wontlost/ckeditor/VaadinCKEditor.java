535a2357226d9ea047855281c17ba48d83947a49
Ryan Pang <ryan.pang@live.com>
2022-09-05 12:49:01 +1200

    Ugprade lit

--- src/main/java/com/wontlost/ckeditor/VaadinCKEditor.java
+++ src/main/java/com/wontlost/ckeditor/VaadinCKEditor.java
@@ -90,7 +90,7 @@ import java.util.logging.Logger;
 @JsModule("./translations/zh.js")
 @JsModule("./translations/zh-cn.js")
 @CssImport("./ckeditor.css")
-@NpmPackage(value = "lit", version = "^2.2.0")
+@NpmPackage(value = "lit", version = "^2.3.1")
 public class VaadinCKEditor extends CustomField<String> implements HasConfig {
 
     private String editorData;
